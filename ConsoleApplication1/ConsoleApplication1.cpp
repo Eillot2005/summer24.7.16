55a6a19ac15819c7bdbd44bb1f6a16c20212f7fe
lyx <doo8opnewman@outlook.com>
2024-07-16 17:08:20 +0800

    添加项目文件。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,42 @@
+﻿//杭州电子科技大学 -1250
+
+//斐波那契+高精度加法问题
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+string addstring(const string& a, const string& b)//高精度加法
+{
+	string result;
+	int a_len = a.length(), b_len = b.length();
+	int carry = 0;
+	int aa = a_len - 1, bb = b_len - 1;
+	while (aa >= 0 || bb >= 0||carry)//a、b的长度不为0或有进位时就继续
+	{
+		int sum = carry;
+		if (aa >= 0)sum += a[aa] - '0';
+		if (bb >= 0)sum += b[bb] - '0';
+		aa -= 1; bb -= 1;
+		carry = sum / 10;//记录下进位
+		result += (sum % 10) + '0';
+	} 
+	reverse(result.begin(), result.end());//翻转
+	return result;
+}
+string fibo(int n)
+{
+	 if (n < 4) {}
+	vector<string> f(n);
+	f[0] = f[1] = f[2] = f[3] = "1";
+	for (int i = 4; i < n; i++)
+	{
+		f[i] = addstring(f[i - 1], addstring(f[i - 2], addstring(f[i - 3], f[i - 4])));
+	}
+	return f[n - 1];
+}
+int  main()
+{
+	int n;
+	while (cin >> n)cout << fibo(n) << endl;
+}
---
+++ summer24.7.16/summer24.7.16.cpp
@@ -0,0 +1,25 @@
+﻿// 杭州电子科技大学 -1200
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	int line;
+	string s;
+	while (cin >> line && line != 0)
+	{
+		cin >> s;
+		for (int i = 0; i < line; i++)
+		{
+			int out = i;
+			for (int j = 1; out < s.length() && j <= s.length() / line; j++)
+			{
+				cout << s[out];
+				if (j % 2)out += 2 * line - 1 - (2 * i);
+				else out += 1 + (2 * i);
+			}
+		}
+		cout << endl;
+	}
+}
